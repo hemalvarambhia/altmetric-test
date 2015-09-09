73e22955707d9b880b756dc33249e4bfaa347418
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-09 15:54:04 +0100

    We cannot have DOI that are initialize with nil

--- 01-problem/spec/doi_spec.rb
+++ 01-problem/spec/doi_spec.rb
@@ -4,6 +4,9 @@ describe "Digital object identifiers" do
 
   class DOI
     def initialize doi_string
+      if doi_string.nil?
+        raise InvalidDOI.new "DOIs cannot be null"
+      end
       if doi_string.empty?
         raise InvalidDOI.new "DOIs cannot be blank"
       end
@@ -13,6 +16,7 @@ describe "Digital object identifiers" do
   describe "a blank DOI" do
     it "is invalid" do
       expect(lambda {DOI.new("")}).to raise_error(InvalidDOI)
+      expect(lambda {DOI.new(nil)}).to raise_error(InvalidDOI)
     end
   end
 end
