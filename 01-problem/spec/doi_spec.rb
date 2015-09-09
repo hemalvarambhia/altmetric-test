c1c430d85b6f5074c7a81dae45f1a96576cbc5b3
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-09 16:25:27 +0100

    A DOI with no suffix is invalid

--- 01-problem/spec/doi_spec.rb
+++ 01-problem/spec/doi_spec.rb
@@ -7,6 +7,8 @@ describe "Digital object identifiers" do
     def valid?
       return false if @doi.empty?
       return false unless @doi.start_with?("10")
+      prefix, suffix = @doi.split("/")
+      return false if suffix.nil?
       true
     end
   end
@@ -21,6 +23,13 @@ describe "Digital object identifiers" do
   describe "a DOI that does not start with 10" do
     it "is not valid" do
       expect(DOI.new("90")).to_not be_valid
+    end
+  end
+
+  describe "a DOI that has a valid prefix and no suffix" do
+    it "is invalid" do
+      expect(DOI.new("10.1234")).to_not be_valid
+      expect(DOI.new("10.1234/")).to_not be_valid
     end
   end
 end
