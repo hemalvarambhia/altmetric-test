ea7496c5e56d97f33dd7adc72bd464fd0e09ed3a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-10 21:38:24 +0100

    A ISSN with no dash should have one added in the right place, added additional test cases and passing production code

--- 01-problem/spec/issn_spec.rb
+++ 01-problem/spec/issn_spec.rb
@@ -4,11 +4,22 @@ describe "ISSNs" do
   
   class ISSN
     def initialize issn_string
-      @issn = issn_string || ""
+      @issn = (issn_string || "").strip
       raise InvalidISSN.new("ISSN cannot be blank") if @issn.empty?
+
       if digits.size != 8
         raise InvalidISSN.new("ISSNs must consist of 8 digits")
       end
+      
+      @issn = @issn.insert(4, "-") if not @issn.include?("-")
+
+      unless @issn=~/^\d{4}-\d{4}$/
+        raise InvalidISSN.new("ISSN '#{@issn}' is badly formed")
+      end
+    end
+
+    def to_s
+      @issn
     end
 
     private 
@@ -34,6 +45,33 @@ describe "ISSNs" do
   describe "an ISSN with more than 8 digits" do
     it "is invalid" do
       expect(lambda {ISSN.new("43565-32932")}).to raise_error(InvalidISSN)
+    end
+  end
+
+  describe "a well-formed ISSN with 8-digits" do
+    it "is valid" do
+      expect(lambda {ISSN.new("0378-5955")}).not_to raise_error
+      expect(lambda {ISSN.new(" 0378-5955 ")}).not_to raise_error
+    end
+
+    it "has only one hash at the 4th position" do
+      expect(ISSN.new("0378-5955").to_s).to eq("0378-5955")
+    end
+  end
+
+  describe "an ISSN with the dash in the wrong position" do
+    it "is invalid" do
+      expect(lambda {ISSN.new("037-85955")}).to raise_error(InvalidISSN)
+    end
+  end
+
+  describe "an 8-digit ISSN without the dash" do
+    it "is valid" do
+       expect(lambda {ISSN.new("03785955")}).not_to raise_error
+    end
+
+    it "includes the dash at instanciation" do
+      expect(ISSN.new("03785955").to_s).to eq("0378-5955")
     end
   end
 
