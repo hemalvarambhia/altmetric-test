7ac7e9b12781abd1bbbd0ef578cdfde62bd94d16
Hemal <h.n.varambhia@gmail.com>
2015-09-11 13:37:45 +0100

    We can render two articles in CSV format

--- 01-problem/spec/rendering_articles_in_csv_spec.rb
+++ 01-problem/spec/rendering_articles_in_csv_spec.rb
@@ -8,13 +8,9 @@ class CSVRenderer
      CSV.generate do |csv|
        csv << header
        article = articles.all.first
-       csv << [
-          article.doi, 
-          article.title, 
-          article.author, 
-          article.journal_published_in.title,
-          article.journal_published_in.issn
-        ] if article
+       csv << as_array(article) if article
+       article = articles.all[1]
+       csv << as_array(article) if article
      end
   end
 
@@ -24,6 +20,15 @@ class CSVRenderer
     ["DOI", "Title", "Author", "Journal Title", "ISSN"]
   end
 
+  def as_array article
+     [
+       article.doi,
+       article.title,
+       article.author,
+       article.journal_published_in.title,
+       article.journal_published_in.issn
+      ]
+  end
 end
 
 describe "Rendering articles to CSV" do
@@ -99,7 +104,51 @@ describe "Rendering articles to CSV" do
   end
 
   describe "rendering 2 articles to CSV" do
-    it "contains the details of both articles"
+    it "contains the details of both articles" do
+      articles = double("Articles")
+      allow(articles).to(
+         receive(:all).and_return(
+           [
+             Article.new(
+               doi: DOI.new("10.1234/altmetric52"),
+               title: "Title of Article",
+               author: "Name of Author",
+               journal: Journal.new(
+                  ISSN.new("3853-8766"),
+                  "Title of Journal")
+             ),
+             Article.new(
+	       doi: DOI.new("10.1234/altmetric101"),
+               title: "Different Title",
+               author: "Different Author",
+               journal: Journal.new(
+	          ISSN.new("6757-2931"),
+                  "Different Journal"
+               )
+             )
+           ]
+         )
+      )
+
+      parsed_csv = CSV.parse(CSVRenderer.new.render(articles))
+      expect(parsed_csv[1]).to(
+          eq(
+             [
+               "10.1234/altmetric52",
+               "Title of Article",
+               "Name of Author",
+               "Title of Journal",
+               "3853-8766"
+             ]))
+      expect(parsed_csv[2]).to(
+             eq([
+               "10.1234/altmetric101",
+               "Different Title",
+               "Different Author", 
+               "Different Journal",
+               "6757-2931"
+             ]))
+    end
   end
 
   describe "rendering many articles to CSV" do
