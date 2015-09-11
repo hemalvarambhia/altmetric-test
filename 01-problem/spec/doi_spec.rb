d4eff82bfa192a8192bed5254e220a617ea72b13
Hemal <h.n.varambhia@gmail.com>
2015-09-11 09:34:17 +0100

    Class was moved to its own script

--- 01-problem/spec/doi_spec.rb
+++ 01-problem/spec/doi_spec.rb
@@ -1,45 +1,5 @@
 require_relative './doi'
 describe "Digital object identifiers" do
-=begin  
-  class DOI
-    def initialize doi_string
-      @doi = doi_string || ""
-    end
-
-    def valid?
-      return false if @doi.empty?
-      
-      return false unless prefix.start_with?("10")
-
-      # The registrant code is currently four digits long,
-      # but this is not syntactically necessary
-      return false unless prefix.match(/10\.\d{4}/)
-
-      return false if suffix.empty?
-
-      return true
-    end
-
-    def to_s
-      components.join("/")
-    end
-
-    private
-    def prefix
-      components[0] || ""
-    end
-
-    def suffix
-      components[1] || ""
-    end
-    
-    def components
-      @doi.split("/").
-        collect{|component| component.strip}.
-        collect{|component| component.gsub(/\s+/, "")}
-    end
-  end
-=end  
   describe "a blank DOI" do
     it "is invalid" do
       expect(DOI.new("")).to_not be_valid
