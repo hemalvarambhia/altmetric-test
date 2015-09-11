404dde9d13e9775e0f27dd06162334df66f6fe84
Hemal <h.n.varambhia@gmail.com>
2015-09-11 12:46:59 +0100

    When there are no articles, the renderer only renders a header

--- 01-problem/spec/rendering_articles_in_csv_spec.rb
+++ 01-problem/spec/rendering_articles_in_csv_spec.rb
@@ -1,11 +1,24 @@
+require 'csv'
 require_relative './issn'
 require_relative './doi'
 require_relative './journal'
 require_relative './article'
+class CSVRenderer
+  def render articles
+    ["DOI", "Title", "Author", "Journal Title", "ISSN"].to_csv
+  end
+
+end
 
 describe "Rendering articles to CSV" do
   describe "rendering no articles to CSV" do
-    it "only contains the headers"
+    it "only contains the headers" do
+      articles = double("Articles")
+      allow(articles).to(receive(:all).and_return([]))
+
+      parsed_csv = CSV.parse(CSVRenderer.new.render(articles))
+      expect(parsed_csv.first).to eq(["DOI", "Title", "Author", "Journal Title", "ISSN"])
+    end
   end
 
   describe "rendering 1 article to CSV" do
