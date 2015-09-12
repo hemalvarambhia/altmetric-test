d69842601661710147a62b4045bf8b8ef87f42a1
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-12 11:34:47 +0100

    We can read 1 article

--- 01-problem/spec/articles.rb
+++ 01-problem/spec/articles.rb
@@ -4,13 +4,25 @@ class Articles
   end
 
   def self.load_from(
-      file_name, journals, articles)
+      file_name, journals, authors)
     if not File.exists?(file_name)
       raise Exception.new(
             "'#{file_name}' does not exist")
     end
-
-    Articles.new([])
+    articles = []
+    CSV.foreach(file_name, {headers: true}) do |csv|
+      doi = DOI.new(csv["DOI"])
+      articles << Article.new(
+        doi: doi,
+        title: csv["Title"],
+        author: authors.author_of(doi),
+        journal: journals.
+          find_journal_for(
+            ISSN.new(csv["ISSN"])))
+    end
+    Articles.new(
+      articles
+    )
   end
 
   def all
--- 01-problem/spec/loading_articles_csv_file_spec.rb
+++ 01-problem/spec/loading_articles_csv_file_spec.rb
@@ -1,3 +1,8 @@
+require_relative './doi'
+require_relative './issn'
+require_relative './journal'
+require_relative './articles'
+
 describe "Loading articles from a CSV file" do
   context "when the file does not exist" do
     it "raises an error" do
@@ -22,6 +27,38 @@ describe "Loading articles from a CSV file" do
       )
       
       expect(articles).to be_empty
+    end
+  end
+
+  context "when the file contains one article" do
+    it "yields the articles" do
+      journals = double("Journals")
+      allow(journals).to(
+        receive(:find_journal_for).
+        with(ISSN.new("1337-8688")).
+        and_return(
+          Journal.new(
+          ISSN.new("1337-8688"),
+          "Shanahan, Green and Ziemann"))
+      )
+      authors = double("Authors")
+      allow(authors).to(
+        receive(:author_of).
+        with(DOI.new("10.1234/altmetric0")))
+      articles = Articles.load_from(
+        File.join(
+        File.dirname(__FILE__),
+        "fixtures",
+        "one_article.csv"),
+        journals,
+        authors
+      )
+      
+      article = articles.all.first
+      expect(article.doi).to eq("10.1234/altmetric0")
+      expect(article.title).to eq("Small Wooden Chair")
+      expect(article.journal_published_in.issn).to eq("1337-8688")
+      expect(article.journal_published_in.title).to eq("Shanahan, Green and Ziemann")
     end
   end
 end
