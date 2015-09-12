4e7d152f877c6aa7040d619744ac1a97590e335b
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-12 11:48:04 +0100

    Moved the arrangement of the test doubles to a set up method to make the test method clearer

--- 01-problem/spec/loading_articles_csv_file_spec.rb
+++ 01-problem/spec/loading_articles_csv_file_spec.rb
@@ -31,9 +31,9 @@ describe "Loading articles from a CSV file" do
   end
 
   context "when the file contains one article" do
-    it "yields the articles" do
-      journals = double("Journals")
-      allow(journals).to(
+    before(:each) do
+      @journals = double("Journals")
+      allow(@journals).to(
         receive(:find_journal_for).
         with(ISSN.new("1337-8688")).
         and_return(
@@ -41,24 +41,32 @@ describe "Loading articles from a CSV file" do
           ISSN.new("1337-8688"),
           "Shanahan, Green and Ziemann"))
       )
-      authors = double("Authors")
-      allow(authors).to(
+
+      @authors = double("Authors")
+      allow(@authors).to(
         receive(:author_of).
-        with(DOI.new("10.1234/altmetric0")))
+        with(DOI.new("10.1234/altmetric0")).
+        and_return("Amari Lubowitz")
+      )
+    end
+    it "yields the articles" do
       articles = Articles.load_from(
         File.join(
         File.dirname(__FILE__),
         "fixtures",
         "one_article.csv"),
-        journals,
-        authors
+        @journals,
+        @authors
       )
       
       article = articles.all.first
       expect(article.doi).to eq("10.1234/altmetric0")
       expect(article.title).to eq("Small Wooden Chair")
-      expect(article.journal_published_in.issn).to eq("1337-8688")
-      expect(article.journal_published_in.title).to eq("Shanahan, Green and Ziemann")
+      expect(article.author).to eq("Amari Lubowitz")
+      expect(article.journal_published_in.issn).to(
+        eq("1337-8688"))
+      expect(article.journal_published_in.title).to(
+        eq("Shanahan, Green and Ziemann"))
     end
   end
 end
