f85e70ea550f07c7c3f98edb94b593db3b3199c0
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-12 16:30:42 +0100

    If there is a journal for the ISSN, we return it

--- 01-problem/spec/journals_spec.rb
+++ 01-problem/spec/journals_spec.rb
@@ -3,14 +3,13 @@ require_relative './journal'
 
 class Journals
   def initialize journals
-    
+    @journals = journals || []
   end
   
-  def find_journal_for issn
-    Journal.new(
-      ISSN.new("1234-5678"),
-      "Journal that exists"
-    )
+  def find_journal_for required_issn
+    @journals.detect{|journal|
+      journal.issn == required_issn
+    }
   end
 end
 
@@ -31,6 +30,25 @@ describe "Finding journals by ISSN" do
 
       expect(journal.title).to eq("Journal that exists")
       expect(journal.issn).to eq(ISSN.new("1234-5678"))
+    end
+
+    context "finding a journal for a different ISSN" do
+      it "is returned" do
+        
+        journals = Journals.new(
+          [
+            Journal.new(
+            ISSN.new("9876-5432"),
+           "Different journal"
+          )])
+
+      journal = journals.find_journal_for(
+                  ISSN.new("9876-5432")
+                )
+
+      expect(journal.title).to eq("Different journal")
+      expect(journal.issn).to eq(ISSN.new("9876-5432"))
+      end
     end
     
   end
