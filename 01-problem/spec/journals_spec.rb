5c5d96446fd3e136b3ca2250563b27518fcfbc05
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-12 16:37:03 +0100

    We return nil when a journal with the required ISSN cannot be found

--- 01-problem/spec/journals_spec.rb
+++ 01-problem/spec/journals_spec.rb
@@ -50,6 +50,22 @@ describe "Finding journals by ISSN" do
       expect(journal.issn).to eq(ISSN.new("9876-5432"))
       end
     end
-    
+  end
+
+  context "when the journal cannot be found" do
+    it "returns no journal" do
+      journals = Journals.new(
+        [
+          Journal.new(
+            ISSN.new("9876-5432"),
+           "Different journal"
+        )
+        ])
+
+      non_existent = journals.find_journal_for(
+        ISSN.new("1234-6878"))
+
+      expect(non_existent).to be_nil
+    end
   end
 end
