c0ab6f806e9855bd757036036beab3f7ba54e153
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-12 14:55:40 +0100

    Test case to confirm we can read off all articles

--- 01-problem/spec/loading_articles_csv_file_spec.rb
+++ 01-problem/spec/loading_articles_csv_file_spec.rb
@@ -146,4 +146,87 @@ describe "Loading articles from a CSV file" do
       expect(article.journal_published_in.issn).to eq("2542-5856")
     end
   end
+
+  context "when the file contains many articles" do
+    before(:each) do
+      @journals = double("Journals")
+      [
+        Journal.new(
+          ISSN.new("1337-8688"),
+          "Shanahan, Green and Ziemann"
+      ),
+        Journal.new(
+          ISSN.new("2542-5856"),
+          "Wilkinson, Gaylord and Gerlach"
+        ),
+        Journal.new(
+          ISSN.new("3775-0307"),
+          "Hahn and Sons"
+        )
+      ].each do |journal|
+        allow(@journals).to(
+          receive(:find_journal_for).
+          with(journal.issn).
+          and_return(journal)
+        )                                          
+       end                                           
+
+      @authors = double("Authors")
+      [
+        Author.new(
+        "Amari Lubowitz",
+         [DOI.new("10.1234/altmetric0")]
+      ),
+         Author.new(
+        "Lenny Kshlerin",
+         [DOI.new("10.1234/altmetric100")]
+        ),
+         Author.new(
+           "Howard Spinka Jr.", 
+           [DOI.new("10.1234/altmetric103")]
+         )       
+      ].each do |author|
+        author.publications.each do |doi|
+          allow(@authors).to(
+            receive(:author_of).
+            with(doi).
+            and_return(author.name)
+          )
+        end
+      end
+    end
+    
+    it "yields every articles" do
+       articles = Articles.load_from(
+        File.join(fixtures_dir, "many.csv"),
+        @journals,
+        @authors
+      ).all
+
+       article = articles.first
+       expect(article.doi).to eq("10.1234/altmetric0")
+       expect(article.title).to eq("Small Wooden Chair")
+       expect(article.author).to eq("Amari Lubowitz")
+       expect(article.journal_published_in.title).to(
+         eq("Shanahan, Green and Ziemann"))
+
+       expect(article.journal_published_in.issn).to eq("1337-8688")
+
+       article = articles.second
+       expect(article.doi).to eq("10.1234/altmetric100")
+       expect(article.title).to eq("Ergonomic Rubber Shirt")
+       expect(article.author).to eq("Lenny Kshlerin")
+       expect(article.journal_published_in.title).to(
+         eq("Wilkinson, Gaylord and Gerlach"))
+       expect(article.journal_published_in.issn).to eq("2542-5856")
+
+       article = articles.last
+       expect(article.doi).to eq("10.1234/altmetric103")
+       expect(article.title).to eq("Fantastic Granite Computer")
+       expect(article.author).to eq("Harry Spink Jr.")
+       expect(article.journal_published_in.title).to(
+         eq("Hahn and Sons"))
+       expect(article.journal_published_in.issn).to eq("3775-0307")
+    end
+  end
 end
