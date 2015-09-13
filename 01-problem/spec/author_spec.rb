e3974840ee5fb1509f20692ca3fcf45d5ce455be
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-13 16:13:00 +0100

    The equals method is symmetric

--- 01-problem/spec/author_spec.rb
+++ 01-problem/spec/author_spec.rb
@@ -24,7 +24,16 @@ describe "Equating Authors" do
     expect(author_1).to eq(author_3)
   end
 
-  it "is symmetric"
+  it "is symmetric" do
+    author_1 = Author.new(
+      "Author", [DOI.new("10.1234/altmetric007")])
+    author_2 = Author.new(
+      "Author", [DOI.new("10.1234/altmetric007")]
+    )
+
+    expect(author_1).to eq author_2
+    expect(author_2).to eq author_2
+  end
 
   context "authors with a different names but same publications" do
     it "confirms them as not being the same"
