f6101010667d31941f7e586f51579053af3f6094
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-13 17:45:52 +0100

    Moved the CSVRenderer to an appropriate place in the source code. Its spec now references it from the new location

---
+++ 01-problem/lib/csv_renderer.rb
@@ -0,0 +1,28 @@
+require 'csv'
+
+class CSVRenderer
+  def render articles
+     CSV.generate do |csv|
+       csv << header
+       articles.all.collect{ |article|
+         csv << as_array(article) if article
+       }
+     end
+  end
+
+  private
+  
+  def header
+    ["DOI", "Title", "Author", "Journal Title", "ISSN"]
+  end
+
+  def as_array article
+     [
+       article.doi,
+       article.title,
+       article.author,
+       article.journal_published_in.title,
+       article.journal_published_in.issn
+      ]
+  end
+end
--- 01-problem/spec/rendering_articles_in_csv_spec.rb
+++ 01-problem/spec/rendering_articles_in_csv_spec.rb
@@ -4,33 +4,7 @@ require_relative '../lib/doi'
 require_relative '../lib/journal'
 require_relative '../lib/article'
 require_relative '../lib/articles'
-
-class CSVRenderer
-  def render articles
-     CSV.generate do |csv|
-       csv << header
-       articles.all.collect{ |article|
-         csv << as_array(article) if article
-       }
-     end
-  end
-
-  private
-  
-  def header
-    ["DOI", "Title", "Author", "Journal Title", "ISSN"]
-  end
-
-  def as_array article
-     [
-       article.doi,
-       article.title,
-       article.author,
-       article.journal_published_in.title,
-       article.journal_published_in.issn
-      ]
-  end
-end
+require_relative '../lib/csv_renderer'
 
 def to_array(articles)
    articles.all.collect do |article|
