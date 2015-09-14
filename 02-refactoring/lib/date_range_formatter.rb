d04134f67ef29c7809f8e40c44f27bba12fa12f0
Hemal <h.n.varambhia@gmail.com>
2015-09-14 13:09:35 +0100

    Duplicate date range prefix code extracted to a method

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -10,36 +10,43 @@ class DateRangeFormatter
   end
 
   def to_s
-    full_start_date = date_in_full(@start_date)
     full_end_date = date_in_full(@end_date)
 
     if both_times_known?
-      return "#{full_start_date} at #{@start_time} to #{@end_time}" if @start_date == @end_date
-      return "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
+      return "#{prefix} to #{@end_time}" if @start_date == @end_date
+      return "#{prefix} - #{full_end_date} at #{@end_time}"
     end
 
     if start_time_known?
-      return "#{full_start_date} at #{@start_time}" if @start_date == @end_date
-      return "#{full_start_date} at #{@start_time} - #{full_end_date}"
+      return "#{prefix}" if @start_date == @end_date
+      return "#{prefix} - #{full_end_date}"
     end
 
     if end_time_known?
-      return "#{full_start_date} until #{@end_time}" if @start_date == @end_date
-      return "#{full_start_date} - #{full_end_date} at #{@end_time}"
+      return "#{prefix} until #{@end_time}" if @start_date == @end_date
+      return "#{prefix} - #{full_end_date} at #{@end_time}"
     end
 
     if @start_date == @end_date
-      full_start_date
+      prefix
     elsif @start_date.month == @end_date.month
       @start_date.strftime("#{@start_date.day.ordinalize} - #{date_in_full(@end_date)}")
     elsif @start_date.year == @end_date.year
       @start_date.strftime("#{@start_date.day.ordinalize} %B - ") + date_in_full(@end_date)
     else
-      "#{full_start_date} - #{full_end_date}"
+      "#{prefix} - #{full_end_date}"
     end
   end
 
   private
+
+  def prefix
+    if start_time_known?
+      "#{date_in_full(@start_date)} at #{@start_time}"
+    else
+      date_in_full(@start_date)
+    end
+  end
 
   def both_times_known?
     start_time_known? and end_time_known?
