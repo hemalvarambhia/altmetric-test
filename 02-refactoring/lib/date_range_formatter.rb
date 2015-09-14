d7c7dfe4d85c2582a8cc56c1f03f4713b50754aa
Hemal <h.n.varambhia@gmail.com>
2015-09-14 12:51:43 +0100

    Collate all logic for when only the start time is known

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -18,26 +18,27 @@ class DateRangeFormatter
       return "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
     end
 
+    if start_time_known?
+      return "#{full_start_date} at #{@start_time}" if @start_date == @end_date
+      return "#{full_start_date} at #{@start_time} - #{full_end_date}" if @start_date.month == @end_date.month
+      return "#{full_start_date} at #{@start_time} - #{full_end_date}" if @start_date.year == @end_date.year
+      return "#{full_start_date} at #{@start_time} - #{full_end_date}"
+    end
+
     if @start_date == @end_date
-      if start_time_known?
-        "#{full_start_date} at #{@start_time}"
-      elsif @end_time
+      if @end_time
         "#{full_start_date} until #{@end_time}"
       else
         full_start_date
       end
     elsif @start_date.month == @end_date.month
-      if start_time_known?
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
+      if @end_time
         "#{full_start_date} - #{full_end_date} at #{@end_time}"
       else
         @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
       end
     elsif @start_date.year == @end_date.year
-      if start_time_known?
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
+      if @end_time
         "#{full_start_date} - #{full_end_date} at #{@end_time}"
       else
         @start_date.strftime("#{@start_date.day.ordinalize} %B - ") + date_in_full(@end_date)
