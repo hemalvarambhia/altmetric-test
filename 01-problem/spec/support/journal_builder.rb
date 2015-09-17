d748a12f2eae16b679894ba853af0cadd0d27f1e
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-17 21:07:58 +0100

    Set the defaults in the factory method

--- 01-problem/spec/support/journal_builder.rb
+++ 01-problem/spec/support/journal_builder.rb
@@ -3,9 +3,7 @@ class JournalBuilder
     JournalBuilder.new(issn, name)
   end
 
-  def initialize(
-        issn = ISSN.new("0387-5955"),
-        name_of_journal = "Physical Review A")
+  def initialize(issn, name_of_journal)
     @issn = issn
     @name = name_of_journal 
   end
