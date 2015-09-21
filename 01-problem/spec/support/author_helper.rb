9abae189611029243a20695f7d9f46e457e7a1c5
Hemal <h.n.varambhia@gmail.com>
2015-09-21 13:17:15 +0100

    Formatting was wrong so corrected

--- 01-problem/spec/support/author_helper.rb
+++ 01-problem/spec/support/author_helper.rb
@@ -3,16 +3,15 @@ require_relative '../../lib/authors'
 require_relative './generate_doi'
 module AuthorHelper
   def write_to authors_file, *authors
+    authors_to_hash = authors.collect { |author|
+      {
+          "name" => author.name,
+          "articles" => author.publications
+      }
+    }
+
     File.open(authors_file, "w") do |file|
-      file.puts <<
-          authors.collect{|author|
-            [
-                {
-                    "name" => author.name,
-                    "articles" => author.publications
-                }
-            ]
-          }.to_json
+      file.puts authors_to_hash.to_json
 
     end
   end
