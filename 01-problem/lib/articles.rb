d881f298ff1b5d92a2081f7e7b24f1c1bffb8abb
Hemal <h.n.varambhia@gmail.com>
2015-09-23 16:35:00 +0100

    Method was renamed

--- 01-problem/lib/articles.rb
+++ 01-problem/lib/articles.rb
@@ -20,7 +20,7 @@ class Articles
     articles = []
     CSV.foreach(file_name, {headers: true}) do |csv|
       doi = DOI.new(csv["DOI"])
-      journal = journals.find_journal_for ISSN.new(csv["ISSN"])
+      journal = journals.find_journal_with ISSN.new(csv["ISSN"])
       article_authors = authors.author_of(doi)
       if journal and article_authors.any?
         articles << Article.new(
