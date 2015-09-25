947986cb867dc1d7644dbce5280c04fb7e5351ce
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-25 22:14:29 +0100

    Corrected rubocop violations

--- 01-problem/lib/authors.rb
+++ 01-problem/lib/authors.rb
@@ -1,6 +1,7 @@
 require 'json'
 require_relative './doi'
 require_relative './author'
+# A class that models a list of authors with journal publication
 class Authors
   include Enumerable
   extend Forwardable
@@ -8,28 +9,28 @@ class Authors
   def_delegator :@authors, :empty?
   def_delegator :@authors, :size
 
-  def initialize(authors)
+  def initialize(authors = [])
     @authors = authors || []
   end
 
-  def each &block
-    @authors.each &block
+  def each(&block)
+    @authors.each(&block)
   end
 
-  def author_of doi
-    Authors.new find_all{ |author| author.published? doi }
+  def author_of(doi)
+    Authors.new select { |author| author.published? doi }
   end
 
-  def self.load_from file_name
-    raise FileNotFound.new(file_name) unless File.exists?(file_name)
+  def self.load_from(file_name)
+    fail FileNotFound, file_name unless File.exist?(file_name)
 
     authors_as_json = JSON.parse(
-        File.open(file_name, "r").read)
-    authors = authors_as_json.collect do |author_as_json|
-      publications = author_as_json["articles"].collect{ |doi| DOI.new(doi) }
-      Author.new(author_as_json["name"], publications)
+        File.open(file_name, 'r').read)
+    authors = authors_as_json.map do |author_as_json|
+      publications = author_as_json['articles'].map { |doi| DOI.new(doi) }
+      Author.new(author_as_json['name'], publications)
     end
 
-    return Authors.new(authors.select{|author| author.has_publications?})
+    Authors.new(authors.select { |author| author.publications.any? })
   end
 end
