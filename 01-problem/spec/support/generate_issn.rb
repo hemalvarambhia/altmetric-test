944e4e57bae3d3197387b29f35444b940f78acfa
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 17:35:09 +0100

    Removed all rubocop violations

--- 01-problem/spec/support/generate_issn.rb
+++ 01-problem/spec/support/generate_issn.rb
@@ -1,9 +1,10 @@
 require_relative '../../lib/issn'
+# A Helper module to generate unique ISSNs
 module GenerateISSN
   def an_issn
     numbers = 0.upto(9).to_a
     parts = [numbers.sample(4).join, numbers.sample(4).join]
-    ISSN.new(
-      "#{parts.first}-#{parts.last}")
+
+    ISSN.new("#{parts.first}-#{parts.last}")
   end
 end
