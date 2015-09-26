8df386ee270ef9d10dbe5ddb24e1728913e5d2ff
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 10:59:16 +0100

    Corrected rubocop violations

--- 01-problem/lib/rendering_factory.rb
+++ 01-problem/lib/rendering_factory.rb
@@ -1,13 +1,13 @@
 require_relative './json_renderer'
 require_relative './csv_renderer'
-
+# A Factory class that creates a rendering object from the given format
 class RenderingFactory
   def renderer_for(format)
     case format
-      when "json"
-        return JSONRenderer.new
-      when "csv"
-        return CSVRenderer.new
-      end
+    when 'json'
+      return JSONRenderer.new
+    when 'csv'
+      return CSVRenderer.new
+    end
   end
 end
