5a65da1d5599c265616f0f40977f67c07ceab4b8
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 15:57:47 +0100

    Corrected all rubocop violations

--- 01-problem/spec/rendering_articles_in_json_spec.rb
+++ 01-problem/spec/rendering_articles_in_json_spec.rb
@@ -1,8 +1,7 @@
 require 'json'
 require_relative './json_rendering_helper'
 
-describe "Rendering articles to JSON" do
+describe 'Rendering articles to JSON' do
   include JSONRenderingHelper
-  it_behaves_like "a renderer"
-
+  it_behaves_like 'a renderer'
 end
