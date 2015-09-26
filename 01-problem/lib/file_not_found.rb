7754a002cd7c12c79cfdfefec5c12003248ecf31
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 10:10:38 +0100

    Rubocop offenses rectified

--- 01-problem/lib/file_not_found.rb
+++ 01-problem/lib/file_not_found.rb
@@ -1,5 +1,6 @@
+# A better intent-revealing exception when a file is not found
 class FileNotFound < Exception
-  def initialize file_name
+  def initialize(file_name)
     super "File '#{file_name}' not found"
   end
 end
