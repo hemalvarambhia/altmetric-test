fde6e6fce3ce98ddf73647653773bcece48cd955
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 13:34:48 +0100

    Corrected all rubocop violations

--- 01-problem/spec/journal_spec.rb
+++ 01-problem/spec/journal_spec.rb
@@ -1,26 +1,26 @@
 require_relative '../lib/journal'
-describe "Equating journals" do
-  it "is reflexive" do
-    journal_1 = Journal.new(ISSN.new("0024-9319"), "Journal")
+describe 'Equating journals' do
+  it 'is reflexive' do
+    journal_1 = Journal.new(ISSN.new('0024-9319'), 'Journal')
 
     expect(journal_1).to eq(journal_1)
   end
 
-  it "is transitive" do
-     journal_1 = Journal.new(ISSN.new("0024-9319"), "A Journal")
-     journal_2 = Journal.new(ISSN.new("0024-9319"), "A Journal")
-     journal_3 = Journal.new(ISSN.new("0024-9319"), "A Journal")
+  it 'is transitive' do
+    journal_1 = Journal.new(ISSN.new('0024-9319'), 'A Journal')
+    journal_2 = Journal.new(ISSN.new('0024-9319'), 'A Journal')
+    journal_3 = Journal.new(ISSN.new('0024-9319'), 'A Journal')
 
-     expect(journal_1).to eq(journal_2)
-     expect(journal_2).to eq(journal_3)
-     expect(journal_1).to eq(journal_3)
+    expect(journal_1).to eq(journal_2)
+    expect(journal_2).to eq(journal_3)
+    expect(journal_1).to eq(journal_3)
   end
 
-  it "is symmetric" do
-     journal_1 = Journal.new(ISSN.new("0032-1478"), "Some Journal")
-     journal_2 = Journal.new(ISSN.new("0032-1478"), "Some Journal")
+  it 'is symmetric' do
+    journal_1 = Journal.new(ISSN.new('0032-1478'), 'Some Journal')
+    journal_2 = Journal.new(ISSN.new('0032-1478'), 'Some Journal')
 
-     expect(journal_1).to eq(journal_2)
-     expect(journal_2).to eq(journal_1)
+    expect(journal_1).to eq(journal_2)
+    expect(journal_2).to eq(journal_1)
   end
 end
