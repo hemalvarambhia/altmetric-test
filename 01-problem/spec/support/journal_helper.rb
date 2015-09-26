1a041ebd70cb394827f2ed9c44c7d1d63f1834a7
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 17:23:17 +0100

    Removed all rubocop violations

--- 01-problem/spec/support/journal_helper.rb
+++ 01-problem/spec/support/journal_helper.rb
@@ -1,19 +1,21 @@
 require_relative '../../lib/journal'
 require_relative './generate_issn'
+# A Helper module for building journals
 module JournalHelper
   include GenerateISSN
 
   def a_journal
-    Builder.new(an_issn, "::Academic Journal::")
+    Builder.new(an_issn, '::Academic Journal::')
   end
-
+  # Builder class to construct authors in a way that
+  # emphasises what is important in the test
   class Builder
     def initialize(issn, name_of_journal)
       @issn = issn
-      @name = name_of_journal 
+      @name = name_of_journal
     end
 
-    def with_issn issn
+    def with_issn(issn)
       @issn = issn
 
       self
