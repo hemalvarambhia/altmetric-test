2013643eeadb7d6af6dd1fec2381065c92665df6
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 17:33:56 +0100

    Corrected all rubocop violations

--- 01-problem/spec/support/generate_doi.rb
+++ 01-problem/spec/support/generate_doi.rb
@@ -1,7 +1,8 @@
 require_relative '../../lib/doi'
+# Helper Module for generating a unique DOI
 module GenerateDOI
   def a_doi
-    suffix = "altmetric#{rand(1000000)}"
+    suffix = "altmetric#{rand(1_000_000)}"
     registrant_code = (0..9).to_a.sample(4).join
     DOI.new("10.#{registrant_code}/#{suffix}")
   end
