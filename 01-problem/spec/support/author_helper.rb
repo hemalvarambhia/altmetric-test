5a31cabf4bb0c79a64fd7a358b1774eae5e4f3fb
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 17:18:15 +0100

    Removed all rubocop violations

--- 01-problem/spec/support/author_helper.rb
+++ 01-problem/spec/support/author_helper.rb
@@ -1,15 +1,17 @@
 require_relative '../../lib/author'
 require_relative './generate_doi'
+# A Helper module for building authors for tests
 module AuthorHelper
   def an_author
     Builder.new
   end
-
+  # Builder class for building Author in a way
+  # that emphasises what is important in a test
   class Builder
     include GenerateDOI
     def initialize
-      @name = "::Author::"
-      @publications = Array.new(3){|index| a_doi}
+      @name = '::Author::'
+      @publications = Array.new(3) { a_doi }
     end
 
     def who_published(*dois)
