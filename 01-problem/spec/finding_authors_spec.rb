ddc1b889c0935a10ea04580372db68cd224fdd23
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 12:24:55 +0100

    Corrected rubocop violations

--- 01-problem/spec/finding_authors_spec.rb
+++ 01-problem/spec/finding_authors_spec.rb
@@ -3,25 +3,25 @@ require_relative '../lib/doi'
 require_relative '../lib/author'
 require_relative '../lib/authors'
 
-describe "Finding authors by their publications" do
+describe 'Finding authors by their publications' do
   include GenerateDOI
-  context "when there is 1 author of the publication" do
-    it "yields the author" do
+  context 'when there is 1 author of the publication' do
+    it 'yields the author' do
       publication = a_doi
       authors = authors(
         an_author,
         an_author,
         an_author.who_published(publication)
       )
-      
+
       authors = authors.author_of publication
 
       expect(authors).to have_published publication
     end
   end
 
-  context "when there are several authors of the publication" do
-    it "yields them all" do
+  context 'when there are several authors of the publication' do
+    it 'yields them all' do
       publication = a_doi
       authors = authors(
         an_author.who_published(a_doi, publication),
@@ -37,8 +37,8 @@ describe "Finding authors by their publications" do
     end
   end
 
-  context "when there are no authors of the publication" do
-    it "yields none" do
+  context 'when there are no authors of the publication' do
+    it 'yields none' do
       publication = a_doi
       authors = authors(an_author, an_author, an_author)
 
@@ -49,13 +49,13 @@ describe "Finding authors by their publications" do
   end
 
   def authors(*builders)
-    Authors.new builders.collect{|builder| builder.build}
+    Authors.new builders.map { |builder| builder.build }
   end
 
   RSpec::Matchers.define :have_published do |publication|
     match do |authors|
       authors.size > 0 &&
-          authors.all?{|author| author.publications.include?(publication)}
+          authors.all? { |author| author.publications.include?(publication) }
     end
   end
 end
