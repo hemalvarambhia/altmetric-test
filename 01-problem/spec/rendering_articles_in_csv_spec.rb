4b142b63044240c76f22ac1dd5be703f8f826918
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 15:55:55 +0100

    Corrected all rubocop violations

--- 01-problem/spec/rendering_articles_in_csv_spec.rb
+++ 01-problem/spec/rendering_articles_in_csv_spec.rb
@@ -2,45 +2,45 @@ require_relative 'spec_helper'
 require 'csv'
 require_relative './csv_rendering_helper'
 
-describe "Rendering articles to CSV" do
+describe 'Rendering articles to CSV' do
   include CSVRenderingHelper
-  it_behaves_like "a renderer"
+  it_behaves_like 'a renderer'
 
-  describe "Rendering no articles in CSV" do
-    it "has the headers" do
+  describe 'Rendering no articles in CSV' do
+    it 'has the headers' do
       articles = Articles.new([])
 
       rendered_articles = CSV.parse(CSVRenderer.new.render(articles))
       expect(rendered_articles.first).to(
           eq(
               [
-                  "DOI",
-                  "Title",
-                  "Author",
-                  "Journal Title",
-                  "ISSN"
+                'DOI',
+                'Title',
+                'Author',
+                'Journal Title',
+                'ISSN'
               ]))
     end
   end
 
-  describe "Rendering 1 article in CSV" do
+  describe 'Rendering 1 article in CSV' do
     before(:each) do
       @all_articles = Articles.new(
           [
-              an_article
-          ].collect{|article| article.build})
+            an_article
+          ].map { |article| article.build })
     end
 
-    it "has a header" do
+    it 'has a header' do
       rendered_articles = CSV.parse(CSVRenderer.new.render(@all_articles))
       expect(rendered_articles[0]).to(
           eq(
               [
-                  "DOI",
-                  "Title",
-                  "Author",
-                  "Journal Title",
-                  "ISSN"
+                'DOI',
+                'Title',
+                'Author',
+                'Journal Title',
+                'ISSN'
               ]))
 
     end
