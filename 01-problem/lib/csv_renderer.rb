ec762cf10935beb59eb67b35205885c3bd2bf031
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-09-26 09:48:36 +0100

    Corrected Rubocop violations

--- 01-problem/lib/csv_renderer.rb
+++ 01-problem/lib/csv_renderer.rb
@@ -1,26 +1,26 @@
 require 'csv'
-
+# Renders Articles to CSV format
 class CSVRenderer
-  def render articles
+  def render(articles)
     CSV.generate do |csv|
       csv << header
-      articles.collect{ |article| csv << as_array(article) }
+      articles.map { |article| csv << as_array(article) }
     end
   end
 
   private
 
   def header
-    ["DOI", "Title", "Author", "Journal Title", "ISSN"]
+    ['DOI', 'Title', 'Author', 'Journal Title', 'ISSN']
   end
 
-  def as_array article
+  def as_array(article)
     [
-        article.doi,
-        article.title,
-        article.author.join(", "),
-        article.journal_published_in.title,
-        article.journal_published_in.issn
+      article.doi,
+      article.title,
+      article.author.join(', '),
+      article.journal_published_in.title,
+      article.journal_published_in.issn
     ]
   end
 end
