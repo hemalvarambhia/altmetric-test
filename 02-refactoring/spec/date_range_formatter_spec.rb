544aa78e2f692c52344ae2708c3c344141fe4719
Hemal <h.n.varambhia@gmail.com>
2015-11-25 16:07:20 +0000

    Grouped examples where only the year is the same in to an appropriate context, added a missing example for when only the end time is specified

--- 02-refactoring/spec/date_range_formatter_spec.rb
+++ 02-refactoring/spec/date_range_formatter_spec.rb
@@ -3,7 +3,7 @@ require "date_range_formatter"
 RSpec.describe(DateRangeFormatter) do
 
   context "when the range begins and ends on the same date" do
-    context "when neither times are known" do
+    context "when neither times are specified" do
       it "takes the form of '<start date>'" do
         formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1")
         expect(formatter.to_s).to eq("1st November 2009")
@@ -33,7 +33,7 @@ RSpec.describe(DateRangeFormatter) do
   end
 
   context 'when the range begins and end within the same year and month' do
-    context 'when neither times are known' do
+    context 'when neither times are specified' do
       it "takes the form of '<start day> - <end day> <month> <year>'" do
         formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3")
         expect(formatter.to_s).to eq("1st - 3rd November 2009")
@@ -62,14 +62,27 @@ RSpec.describe(DateRangeFormatter) do
     end
   end
 
-  it "formats a date range for the same year" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-12-1")
-    expect(formatter.to_s).to eq("1st November - 1st December 2009")
-  end
+  context 'when the date range is within the same year' do
+    context 'when neither times are specified' do
+      it "takes the form '<start day> <start month> - <end day> <end month> <year>'" do
+        formatter = DateRangeFormatter.new("2009-11-1", "2009-12-1")
+        expect(formatter.to_s).to eq("1st November - 1st December 2009")
+      end
+    end
 
-  it "formats a date range for the same year with starting time" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-12-1", "10:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 1st December 2009")
+    context 'when only the start time is specified' do
+      it "takes the form '<start date> at <start time> - <end date>'" do
+        formatter = DateRangeFormatter.new("2009-11-1", "2009-12-1", "10:00")
+        expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 1st December 2009")
+      end
+    end
+
+    context 'when only the end time is specified' do
+      it "takes the form '<start date> - <end date> at <end time>'" do
+        formatter = DateRangeFormatter.new("2009-10-2", "2009-12-5", nil, "11:00")
+        expect(formatter.to_s).to eq("2nd October 2009 - 5th December 2009 at 11:00")
+      end
+    end
   end
 
   it "formats a date range for the same year with starting and ending times" do
