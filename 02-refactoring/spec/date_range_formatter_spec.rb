fd3f93a1fb7919103a983c440f16a26d88ee1f1d
Hemal <h.n.varambhia@gmail.com>
2015-11-25 14:32:14 +0000

    Moved the example to the 'same start & end date' context and improved the description of the behaviour

--- 02-refactoring/spec/date_range_formatter_spec.rb
+++ 02-refactoring/spec/date_range_formatter_spec.rb
@@ -23,11 +23,13 @@ RSpec.describe(DateRangeFormatter) do
         expect(formatter.to_s).to eq("3rd November 2010 until 13:00")
       end
     end
-  end
 
-  it "formats a date range for the same day with starting and ending times" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00", "11:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00 to 11:00")
+    context 'when both times are specified' do
+      it "formats the date with start and end times" do
+        formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00", "11:00")
+        expect(formatter.to_s).to eq("1st November 2009 at 10:00 to 11:00")
+      end
+    end
   end
 
   it "formats a date range for the same month" do
