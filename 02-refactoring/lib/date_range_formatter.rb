30453566afc109b1403e3c2fc3c7979487742494
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-11-26 17:09:04 +0000

    Grouped logic for formatting date ranges when both times are known

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -10,13 +10,15 @@ class DateRangeFormatter
   end
 
   def to_s
-    full_start_date = @start_date.strftime("#{@start_date.day.ordinalize} %B %Y")
-    full_end_date = @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
-
+    if @start_time && @end_time
+      return  "#{full_start_date} at #{@start_time} to #{@end_time}" if @start_date == @end_date
+      return  "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}" if @start_date.month == @end_date.month
+      return  "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}" if @start_date.year == @end_date.year
+      return "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
+    end
+    
     if @start_date == @end_date
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} to #{@end_time}"
-      elsif @start_time
+      if @start_time
         "#{full_start_date} at #{@start_time}"
       elsif @end_time
         "#{full_start_date} until #{@end_time}"
@@ -24,9 +26,7 @@ class DateRangeFormatter
         full_start_date
       end
     elsif @start_date.month == @end_date.month
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
+      if @start_time
         "#{full_start_date} at #{@start_time} - #{full_end_date}"
       elsif @end_time
         "#{full_start_date} - #{full_end_date} at #{@end_time}"
@@ -34,9 +34,7 @@ class DateRangeFormatter
         @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
       end
     elsif @start_date.year == @end_date.year
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
+      if @start_time
         "#{full_start_date} at #{@start_time} - #{full_end_date}"
       elsif @end_time
         "#{full_start_date} - #{full_end_date} at #{@end_time}"
@@ -44,9 +42,7 @@ class DateRangeFormatter
         @start_date.strftime("#{@start_date.day.ordinalize} %B - ") + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
       end
     else
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
+      if @start_time
         "#{full_start_date} at #{@start_time} - #{full_end_date}"
       elsif @end_time
         "#{full_start_date} - #{full_end_date} at #{@end_time}"
@@ -54,6 +50,16 @@ class DateRangeFormatter
         "#{full_start_date} - #{full_end_date}"
       end
     end
+  end
+
+  private
+
+  def full_start_date
+    @start_date.strftime("#{@start_date.day.ordinalize} %B %Y")
+  end
+
+  def full_end_date
+    @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
   end
 end
 
