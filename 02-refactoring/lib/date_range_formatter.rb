c4dd6fdcb8dea1ac74ca09494a790bf2623092fc
Hemal <h.n.varambhia@gmail.com>
2015-11-27 11:49:34 +0000

    Grouped all logic for when dates are exactly the same

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -10,10 +10,16 @@ class DateRangeFormatter
   end
 
   def to_s
+    if @start_date == @end_date
+      return "#{format_prefix} to #{@end_time}" if @start_time && @end_time
+      return "#{format_prefix}" if @start_time
+      return "#{format_prefix} until #{@end_time}" if @end_time
+      return format_suffix
+    end
+
     return full_format if @start_time && @end_time
     return format_with_start_time if @start_time
     return format_with_end_time if @end_time
-    return format_suffix if @start_date == @end_date
     return @start_date.strftime("#{@start_date.day.ordinalize}") +" - "+ full_end_date if @start_date.month == @end_date.month
     return @start_date.strftime("#{@start_date.day.ordinalize} %B") + " - " + full_end_date if @start_date.year == @end_date.year
 
@@ -23,22 +29,14 @@ class DateRangeFormatter
   private
 
   def full_format
-    if @start_date == @end_date
-      return "#{format_prefix} to #{@end_time}"
-    end
-    
     "#{format_prefix} - #{format_suffix}"
   end
 
   def format_with_start_time
-    return "#{format_prefix}" if @start_date == @end_date
-
     "#{format_prefix} - #{format_suffix}"
   end
 
   def format_with_end_time
-    return "#{format_prefix} until #{@end_time}" if @start_date == @end_date
-
     "#{format_prefix} - #{format_suffix}"
   end
 
