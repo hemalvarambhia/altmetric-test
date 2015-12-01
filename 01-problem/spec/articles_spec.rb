0d23871a8243a103d99058f2800f0328aa5185e6
Hemal <h.n.varambhia@gmail.com>
2015-12-01 11:53:52 +0000

    Already tested elsewhere

--- 01-problem/spec/articles_spec.rb
+++ 01-problem/spec/articles_spec.rb
@@ -4,14 +4,4 @@ describe Articles do
   it 'is initially empty' do
      expect(Articles.new).to be_empty
   end
-
-  describe 'loading articles from a CSV file' do
-    context 'when the file does not exist' do
-      it 'raises an error' do
-        expect(-> { Articles.load_from('non_existent.csv') }).to(
-          raise_error(FileNotFound)
-        )
-      end
-    end
-  end
 end
