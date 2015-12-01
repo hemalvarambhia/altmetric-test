fd38d71b39853e6819a3fd68fd366e9ce54b07ab
Hemal <h.n.varambhia@gmail.com>
2015-12-01 17:18:54 +0000

    Moved the factory method into the Rendering module

--- 01-problem/combine.rb
+++ 01-problem/combine.rb
@@ -19,6 +19,5 @@ journal_csv, articles_csv, authors_json = [ARGV[0], ARGV[1], ARGV[2]]
 journals = Journals.from_file(journal_csv)
 authors = Authors.from_file(authors_json)
 articles = Articles.from_file(articles_csv, journals, authors)
-rendering_factory = RenderingFactory.new
-renderer = rendering_factory.renderer_for format
+renderer = Rendering.renderer_for format
 puts renderer.render articles
--- 01-problem/lib/rendering_factory.rb
+++ 01-problem/lib/rendering_factory.rb
@@ -1,13 +1,13 @@
 require_relative './json_renderer'
 require_relative './csv_renderer'
 # A Factory class that creates a rendering object from the given format
-class RenderingFactory
-  def renderer_for(format)
+module Rendering
+  def self.renderer_for(format)
     case format
-    when 'json'
-      return Rendering::AsJSON.new
-    when 'csv'
-      return Rendering::AsCSV.new
+      when 'json'
+        return Rendering::AsJSON.new
+      when 'csv'
+        return Rendering::AsCSV.new
     end
   end
 end
